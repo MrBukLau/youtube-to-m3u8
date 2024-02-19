3a2b296b0ba3112a7e05f9b00eadc9ddccc5301a
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2024-02-19 10:46:27 -0800

    Add files via upload

--- scripts/setnewsplus.py
+++ scripts/setnewsplus.py
@@ -5,50 +5,48 @@ import os
 import sys
 
 windows = False
-if 'win' in sys.platform:
+if "win" in sys.platform:
     windows = True
 
 def grab(url):
     response = s.get(url, timeout=15).text
-    if '.m3u8' not in response:
+    if ".m3u8" not in response:
         response = requests.get(url).text
-        if '.m3u8' not in response:
+        if ".m3u8" not in response:
             if windows:
-                print('https://raw.githubusercontent.com/MrBukLau/youtube-to-m3u8/main/assets/info.m3u8')
+                print("https://raw.githubusercontent.com/MrBukLau/youtube-to-m3u8/main/unavailable.m3u8")
                 return
-            #os.system(f'wget {url} -O temp.txt')
-            os.system(f'curl "{url}" > temp.txt')
-            response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/MrBukLau/youtube-to-m3u8/main/assets/info.m3u8')
+            #os.system(f"wget {url} -O temp.txt")
+            os.system(f"curl '{url}' > temp.txt")
+            response = "".join(open("temp.txt").readlines())
+            if ".m3u8" not in response:
+                print("https://raw.githubusercontent.com/MrBukLau/youtube-to-m3u8/main/unavailable.m3u8")
                 return
-    end = response.find('.m3u8') + 5
+    end = response.find(".m3u8") + 5
     tuner = 100
     while True:
-        if 'https://' in response[end-tuner : end]:
+        if "https://" in response[end-tuner : end]:
             link = response[end-tuner : end]
-            start = link.find('https://')
-            end = link.find('.m3u8') + 5
+            start = link.find("https://")
+            end = link.find(".m3u8") + 5
             break
         else:
             tuner += 5
-    streams = s.get(link[start:end]).text.split('#EXT')
+    streams = s.get(link[start:end]).text.split("#EXT")
     hd = streams[-1].strip()
-    st = hd.find('http')
+    st = hd.find("http")
     print(hd[st:].strip())
     #print(f"{link[start : end]}")
 
-print('#EXTM3U')
-print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5400000')
+print("#EXTM3U")
 s = requests.Session()
-with open('../information/setnewsplus.txt') as f:
+with open("../information/setnewsplus.txt") as f:
     for line in f:
         line = line.strip()
-        if not line or line.startswith('~~'):
+        if not line or line.startswith("~~"):
             continue
-        if not line.startswith('https:'):
-            line = line.split('|')
+        if not line.startswith("https:"):
+            line = line.split("|")
             ch_name = line[0].strip()
             grp_title = line[1].strip().title()
             tvg_logo = line[2].strip()
@@ -56,6 +54,6 @@ with open('../information/setnewsplus.txt') as f:
         else:
             grab(line)
 
-if 'temp.txt' in os.listdir():
-    os.system('rm temp.txt')
-    os.system('rm watch*')
+if "temp.txt" in os.listdir():
+    os.system("rm temp.txt")
+    os.system("rm watch*")
