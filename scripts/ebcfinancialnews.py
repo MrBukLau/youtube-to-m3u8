bd8255ae13fb89299a2540372a946b7cfe017c69
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2024-02-19 19:35:42 -0800

    Add files via upload

--- scripts/ebcfinancialnews.py
+++ scripts/ebcfinancialnews.py
@@ -14,13 +14,10 @@ def grab(url):
         response = requests.get(url).text
         if ".m3u8" not in response:
             if windows:
-                print("https://lnc-bob-ross.tubi.video/playlistR1080p.m3u8")
                 return
-            #os.system(f"wget {url} -O temp.txt")
             os.system(f"curl '{url}' > temp.txt")
             response = "".join(open("temp.txt").readlines())
             if ".m3u8" not in response:
-                print("https://lnc-bob-ross.tubi.video/playlistR1080p.m3u8")
                 return
     end = response.find(".m3u8") + 5
     tuner = 100
@@ -36,7 +33,6 @@ def grab(url):
     hd = streams[-1].strip()
     st = hd.find("http")
     print(hd[st:].strip())
-    #print(f"{link[start : end]}")
 
 print("#EXTM3U")
 print("#EXT-X-STREAM-INF:BANDWIDTH=10000000")
