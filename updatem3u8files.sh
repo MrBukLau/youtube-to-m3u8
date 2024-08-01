1bd815d2a376d8c8e7f5d93de8ab8e1bf6819dc1
Buk Lau <38795741+MrBukLau@users.noreply.github.com>
2024-08-01 11:29:56 -0700

    Add files via upload

--- updatem3u8files.sh
+++ updatem3u8files.sh
@@ -4,8 +4,7 @@ python3 -m pip install requests
 cd $(dirname $0)/scripts/
 python3 8worldsg.py > ../streams/8worldsg.m3u8
 python3 asiasatellitetelevision.py > ../streams/asiasatellitetelevision.m3u8
-python3 battleoflove.py > ../streams/battleoflove.m3u8
-python3 caravancdrama.py > ../streams/caravancdrama.m3u8
+python3 believeinfuturetv.py > ../streams/believeinfuturetv.m3u8
 python3 cctv4.py > ../streams/cctv4.m3u8
 python3 cctvdocumentary.py > ../streams/cctvdocumentary.m3u8
 python3 cctvdrama.py > ../streams/cctvdrama.m3u8
@@ -30,7 +29,6 @@ python3 etmallch34.py > ../streams/etmallch34.m3u8
 python3 etmallch46.py > ../streams/etmallch46.m3u8
 python3 etmallch47.py > ../streams/etmallch47.m3u8
 python3 etmallch60.py > ../streams/etmallch60.m3u8
-python3 ettodaynews.py > ../streams/ettodaynews.m3u8
 python3 ftvdrama.py > ../streams/ftvdrama.m3u8
 python3 ftvnews.py > ../streams/ftvnews.m3u8
 python3 globalfinancialnews.py > ../streams/globalfinancialnews.m3u8
@@ -38,12 +36,15 @@ python3 globalnews.py > ../streams/globalnews.m3u8
 python3 globalnewstaiwan.py > ../streams/globalnewstaiwan.m3u8
 python3 gtvdrama.py > ../streams/gtvdrama.m3u8
 python3 hairunmedia.py > ../streams/hairunmedia.m3u8
+python3 hopsports.py > ../streams/hopsports.m3u8
 python3 hungergames.py > ../streams/hungergames.m3u8
 python3 iqiyi.py > ../streams/iqiyi.m3u8
 python3 iqiyiactionmovie.py > ../streams/iqiyiactionmovie.m3u8
+python3 iqiyianime.py > ../streams/iqiyianime.m3u8
 python3 iqiyicdrama.py > ../streams/iqiyicdrama.m3u8
 python3 iqiyicomedytheater.py > ../streams/iqiyicomedytheater.m3u8
 python3 iqiyikungfumovie.py > ../streams/iqiyikungfumovie.m3u8
+python3 iqiyimidnighttheater.py > ../streams/iqiyimidnighttheater.m3u8
 python3 iqiyimovieenglish.py > ../streams/iqiyimovieenglish.m3u8
 python3 iqiyimovietheater.py > ../streams/iqiyimovietheater.m3u8
 python3 iqiyiromance.py > ../streams/iqiyiromance.m3u8
@@ -59,6 +60,7 @@ python3 momoch48.py > ../streams/momoch48.m3u8
 python3 ntdaptv.py > ../streams/ntdaptv.m3u8
 python3 phoenixinfonews.py > ../streams/phoenixinfonews.m3u8
 python3 ptsnews.py > ../streams/ptsnews.m3u8
+python3 sandacabletv.py > ../streams/sandacabletv.m3u8
 python3 seeingshanghai.py > ../streams/seeingshanghai.m3u8
 python3 setdrama.py > ../streams/setdrama.m3u8
 python3 setinews.py > ../streams/setinews.m3u8
@@ -66,8 +68,6 @@ python3 setlivenews.py > ../streams/setlivenews.m3u8
 python3 setnews.py > ../streams/setnews.m3u8
 python3 setnewsplus.py > ../streams/setnewsplus.m3u8
 python3 shanghaitv.py > ../streams/shanghaitv.m3u8
-python3 studio886tw.py > ../streams/studio886tw.m3u8
-python3 supercomedyleague.py > ../streams/supercomedyleague.m3u8
 python3 taiwanpluslive.py > ../streams/taiwanpluslive.m3u8
 python3 ttvclassic.py > ../streams/ttvclassic.m3u8
 python3 ttvnews.py > ../streams/ttvnews.m3u8
